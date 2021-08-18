a661cc528e05257461841a6b3a5c3e0fa0f1e4ea
Paul Engel <pm_engel@icloud.com>
2021-08-18 11:57:02 +0200

    chore: Clean up server.js a bit

--- server.js
+++ server.js
@@ -3,31 +3,32 @@ const fs = require('fs');
 const path = require('path');
 const port = 9797;
 
-const pathPrefix = '/bettyblocks/json-schema/master'
+const pathPrefix = '/bettyblocks/json-schema/master';
 
-http.createServer(function (request, response) {
-    console.log('Request for: ', request.url);
-    filePath = request.url.replace(pathPrefix, '');
-    fullPath = path.join(process.cwd(), filePath);
+http.createServer((request, response) => {
 
-    console.log('Serving: ', fullPath);
+  console.log('Request for: ', request.url);
 
-    fs.readFile(fullPath, function(error, content) {
-      if (error) {
-        if(error.code == 'ENOENT'){
-          response.writeHead(404);
-          response.end('Not Found');
-        }
-        else {
-          response.writeHead(500);
-          response.end('Internal Error: '+error.code+' ..\n');
-        }
+  filePath = request.url.replace(pathPrefix, '');
+  fullPath = path.join(process.cwd(), filePath);
+
+  console.log('Serving: ', fullPath);
+
+  fs.readFile(fullPath, function(error, content) {
+    if (error) {
+      if (error.code == 'ENOENT') {
+        response.writeHead(404);
+        response.end('Not Found');
+      } else {
+        response.writeHead(500);
+        response.end('Internal Error: ' + error.code + ' ..\n');
       }
-      else {
-        response.writeHead(200);
-        response.end(content, 'utf-8');
-      }
-    });
+    } else {
+      response.writeHead(200);
+      response.end(content, 'utf-8');
+    }
+  });
 
 }).listen(port);
-console.log(`Server running at http://127.0.0.1:/${port}`);
+
+console.log(`Server running at http://127.0.0.1:${port}`);
